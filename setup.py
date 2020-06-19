c00612a467fdc942445b257d87c54800425a6c56
Kyle Godbey <me@kyle.ee>
2020-06-19 12:50:41 -0500

    make it a pip install

---
+++ setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+"""Distutils setup.py."""
+
+from setuptools import setup, find_packages
+
+from quantumlib import __version__
+
+setup(
+    name='quantumlib',
+    version=__version__,
+    description='Fork of IBM Quantum Challenge API',
+    long_description='Fork of IBM Quantum Challenge API implementing changes for a more cutesy visualizer',
+    url='',
+    author='IBM Quantum Community Team + Kyle Godbey',
+    author_email='me@kyle.ee',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6.9",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords="iqx quantum flask challenge may 4th anniversary birthday",
+    packages=find_packages(include=[
+        'quantumlib',
+        'quantumlib.*',
+        'qlib_common',
+        'qlib_common.*'
+    ]),
+    install_requires=[
+        'seaborn',
+        'requests',
+        'qiskit-terra>=0.13,<0.14',
+        'qiskit-aer>=0.5,<0.6',
+        'numpy',
+        'ipython',
+        'ipywidgets',
+        'matplotlib'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.6.9",
+)
